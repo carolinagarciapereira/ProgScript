b47528f9a88323eb3be46d66f93ba76a354d510b
carolinagarciapereira <141181051+carolinagarciapereira@users.noreply.github.com>
2025-03-31 23:16:56 -0300

    Update PastelCardapio.js

--- app/components/PastelCardapio.js
+++ app/components/PastelCardapio.js
@@ -2,17 +2,17 @@ import styles from "@/app/pastelaria/css/style.module.css";
 import Image from "next/image";
 import If from "@/app/components/If";
 
-export default function ItemMenu({ nome, preco }) {
-    const riscado = nome.includes("Pizza");
-    const imagemSrc = "/image/pastel.jpeg";
+export default function PastelCardapio({ nome, preco }) {
+    const semEstoque = nome.includes("Pizza");
+    const imagemSrc = "@/public/pastel.png";
 
     return (
         <div className={styles.itemMenu}>
-            <If teste={riscado}>
-                <p className={styles.riscado}>{nome}</p>
+            <If teste={semEstoque}>
+                <p className={styles.semEstoque}>{nome}</p>
                 <Image className={styles.grayscale} src={imagemSrc} alt={nome} width={80} height={80} />
             </If>
-            <If teste={!riscado}>
+            <If teste={!semEstoque}>
                 <p>{nome}</p>
                 <Image src={imagemSrc} alt={nome} width={80} height={80} />
             </If>
